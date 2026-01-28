08aed6ed884922edff9c0f25d7f45e8d6fdd6504
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-01-28 13:27:22 -0300

    feat : product section / product card ( still in progress ) / types / product grid / categories tiles section

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import { FEATURED_PRODUCTS_QUERY_RESULT } from "./sanity.types";
+import { ALL_CATEGORIES_QUERY_RESULT, FEATURED_PRODUCTS_QUERY_RESULT, FILTER_PRODUCTS_BY_NAME_QUERY_RESULT } from "./sanity.types";
 
 export type FeaturedProduct = FEATURED_PRODUCTS_QUERY_RESULT[number]; // number refers to a singular instance of the type array
 
@@ -10,3 +10,27 @@ export type FeaturedCarouselProps = {
   products: FEATURED_PRODUCTS_QUERY_RESULT;
 }
 
+export type CategoryTilesProps = {
+  categories: ALL_CATEGORIES_QUERY_RESULT;
+  activeCategory?: string;
+}
+
+export type ProductSectionProps = {
+  categories: ALL_CATEGORIES_QUERY_RESULT;
+  products: FILTER_PRODUCTS_BY_NAME_QUERY_RESULT;
+  searchQuery: string;
+}
+
+export type ProductFiltersProps = {
+  categories: ALL_CATEGORIES_QUERY_RESULT;
+}
+
+export type ProductGridProps = {
+  products: FILTER_PRODUCTS_BY_NAME_QUERY_RESULT;
+}
+
+type Product = FILTER_PRODUCTS_BY_NAME_QUERY_RESULT[number];
+
+export type ProductCardProps = {
+  product: Product;
+}
